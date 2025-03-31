42d98028069f9585cc7446be823ff0e6aedbf36f
Sreedevi0602 <sreedevivs1002@gmail.com>
2025-04-01 03:32:19 +0530

    book listing page, products added, book detail page

--- aloud/myapp/urls.py
+++ aloud/myapp/urls.py
@@ -5,4 +5,6 @@ from myapp.views import *
 urlpatterns = [
     path('addbook/', addbook, name='addbookurl'),  
     path('booklist/', booklist, name='booklisturl'),  
+    path('api/books/', book_list, name='book-list-url'),
+    path('api/books/<str:bookid>/', views.book_detail, name='book-detail-url')
 ]
--- aloud/myapp/views.py
+++ aloud/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from . models import *
 from rest_framework.response import Response
 from . serializer import *
@@ -14,7 +15,7 @@ def addbook(request):
         form = BooksForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('booklist')  
+            return redirect('booklisturl')  
     else:
         form = BooksForm()
     
@@ -24,3 +25,20 @@ def booklist(request):
     books = Books.objects.all()
     return render(request, 'booklist.html', {'books': books})
     
+@api_view(['GET'])
+def book_list(request):
+    books = Books.objects.all()
+    serializer = BooksSerializer(books, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def book_detail(request, bookid):
+    try:
+        # Fetch the book by its bookid (which is a string)
+        book = Books.objects.get(bookid=bookid)
+    except Books.DoesNotExist:
+        return Response(status=404)  # Return 404 if the book doesn't exist
+
+    # Serialize the book details and return it in the response
+    serializer = BooksSerializer(book)
+    return Response(serializer.data)
